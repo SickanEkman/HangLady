cf9b39a25f90a8c5631c66219f834337ed6ac189
Sara Ekman <sara.d.ekman@gmail.com>
2017-08-16 16:24:51 +0200

    Cleaned up

--- hang_lady.py
+++ hang_lady.py
@@ -6,17 +6,12 @@ Created on Tue May 16 01:38:23 2017
 @author: sara
 """
 #import-statements here
-
 import re
 from hang_lady_prepare_game import game_d #dictionary with '_' as values
 from hang_lady_prepare_game import corpus_d #dictionary with word as k, frequency as v
 
-def word_output():
-    presentation, numbers_under_joined = show_word(game_d) # creates nice output from dictionary
-    print("\n" + presentation) # shows right number '_' to user
-    print(numbers_under_joined) #shows count under each '_'
-
-def show_word(game_d):
+#functions
+def show_word(game_d): #function called from word_output()
     """Creates output of word and '_' to print"""
     x = 1
     numbers_under = []
@@ -28,115 +23,102 @@ def show_word(game_d):
     letters_joined = ' '.join(letters)
     return letters_joined, numbers_under_joined
 
-def letter_freq():
+def word_output(game_d):
+    letters_joined, numbers_under_joined = show_word(game_d) # creates nice output from dictionary
+    print("\nThis is your word:")
+    print(letters_joined) # shows right number '_' to user
+    print(numbers_under_joined + "\n") #shows count under each '_'
+
+def create_regexp(game_d):
+    """Creates regexp for letter IN word, from game dictionary"""
+    regexp_l = []
+    for k,v in game_d.items(): #game_d = {1: '_', 2: '_' ...}
+        if v == "_":
+            regexp_l.append(".") #every "_" added to regexp_l as a "."
+        else:
+            regexp_l.append(v) #every letter added to regexp_l as the letter
+    regexp = "".join(regexp_l) #creates a string from regexp_l
+    return regexp #returns a regexp (type str)
+
+def mod_corpus_d(regexp, corpus_d):
+    """Deletes all entries in corpus dictionary that doesn't match regexp"""
+    delete_l = []
+    for k,v in corpus_d.items(): #for every line in corpus_d
+        if not re.match(regexp, k): #if corpus_d key doesn't match my regular expression
+            delete_l.append(k) #add the key to delete_l
+        else:
+            pass #if the key match the regular expression - do nothing
+    for i in delete_l:
+        del corpus_d[i] #delete all entries in corpus_d that correspond to item in delete_l
+    return corpus_d #returns a reduced corpus_d, only containing words that matches regular expression
+
+def letter_freq(corpus_d, used_letters):
     """counts letter freq in corpus dictionary, returns most common letter"""
     freq_d = {} # dictionary with letters as keys and frequencies as values
-    for i in corpus_d:
-        entry = i * corpus_d.get(i) # counts the key, 'value' times
+    for i in corpus_d: #for every entry in corpus_d
+        entry = i * corpus_d.get(i) # entry = the key, 'value' times (e.g. "cowcowcow")
         for letter in entry:
-            if letter in used_letters:
+            if letter in used_letters: #if letter har already been guessed (this shouldn't happen?)
                 pass
             elif letter not in freq_d:
-                freq_d[letter] = 1
+                freq_d[letter] = 1 # adds entry {letter: 1}
             else:
-                freq_d[letter] = freq_d[letter] + 1
-    frequency_list = [(k, freq_d[k]) for k in sorted(freq_d, key=freq_d.get, reverse=True)]
-    # print(len(frequency_list), frequency_list)
-    # print(frequency_list[0][0])
-    if frequency_list[0][0] not in used_letters:
-        used_letters.append(frequency_list[0][0])
-    return frequency_list[0][0]
+                freq_d[letter] = freq_d[letter] + 1 #adds 1 to value = {letter: 2}(1+1)
+    frequency_list = [(k, freq_d[k]) for k in sorted(freq_d, key=freq_d.get, reverse=True)] 
+    #sorts freq_d after value, in reversed order. I.e. first entry = most frequent letter
+    if frequency_list[0][0] not in used_letters: 
+        used_letters.append(frequency_list[0][0]) #adds most frequent letter to list used_letters
+    return frequency_list[0][0], used_letters
+
+def create_miss_regexp(wrong_letter):
+    """Creates regexp for letter NOT in word from make_guess"""
+    regexp = "[^" + wrong_letter + "]"
+    return regexp
 
 def make_guess(most_common_letter):
     """Asks if letter's in word, creates a list with placements if yes"""
-    placement_l = [] #list of places in word where letter occurs
-    yesno = input("Is '" + most_common_letter + "' in the word?\nAnswer 'yes' or 'no'\n")
-    if yesno.strip() == "yes" or yesno == "YES" or yesno == "Yes":
-        num_dots = regexp.count(".") #counts number of unknowned letters in word
-        if num_dots == 1: #if only one unknowned letter left, and player confirmed guess
-        # TODO: return something instead of placement_l to end the game!!! Done?
-            return 1, "done"
-        else:
-            placement_question = input("Where in the word is the letter? Write the number, or numbers - separated by whitespace - if it's in more than one place.\n")
-            placement_l = placement_question.split()
-            return 1, placement_l
-    elif yesno.strip() == "no" or yesno == "NO" or yesno == "No":
-        return 0, create_miss_regexp(most_common_letter)
-    else:
+    yesno = input("Is the letter '" + most_common_letter + "' in the word?\nAnswer 'yes' or 'no'\n")
+    if yesno.strip() == "yes" or yesno == "YES" or yesno == "Yes": #if answer is yes
+        num_dots = regexp.count(".") #counts number of unknowned letters in regular expression
+        if num_dots == 1: #if this is the last unknowned letter
+            return "yes", "done"
+        else: #if there are more than one unknowned letter
+            placement_question = input("Where is the letter? Write the number. Or numbers, separated by whitespace, if it's in more than one place.\n")
+            return "yes", placement_question
+    elif yesno.strip() == "no" or yesno == "NO" or yesno == "No": #if answer is no
+        return "no", create_miss_regexp(most_common_letter) #returns regular expression for wrong letter
+    else: #if answer is neither yes or no
         print("I didn't get that.")
-        return make_guess(most_common_letter)
+        return make_guess(most_common_letter) #starts over with function make_guess
 
 def mod_game_d(placement_l, most_common_letter):
     """Modifies the game dictionary with any new correct letters"""
     for i in placement_l:
         i = int(i)
         game_d[i] = most_common_letter
-    print(game_d[i])
     return(game_d)
-
-def create_regexp(game_d):
-    """Creates regexp for letter IN word, from game dictionary"""
-    regexp_l = []
-    for k,v in game_d.items():
-        if v == "_":
-            regexp_l.append(".")
-        else:
-            regexp_l.append(v)
-    regexp = "".join(regexp_l)
-    print("This is the regexp:", regexp)
-    return regexp
-
-def create_miss_regexp(wrong_letter):
-    """Creates regexp for letter NOT in word from make_guess"""
-    regexp = "[^" + wrong_letter + "]"
-    print("This is the error-regexp:", regexp)
-    return regexp
-
-def mod_corpus_d(regexp, corpus_d):
-    """Deletes all entries in corpus dictionary that doesn't match regexp"""
-    delete_l = []
-    expression = re.compile(regexp)
-    for k,v in corpus_d.items():
-        if not re.match(expression, k):
-            delete_l.append(k)
-            #print(k)
-        else:
-            pass
-    for i in delete_l:
-        del corpus_d[i]
-    return corpus_d
 
 # Initializing variables
 num_guesses = 0
 used_letters = []
 regexp = "."
 
-#game starts with import from hang_lady_prepare_game up top
-
-# main
+# Main
 while "." in regexp:
-    word_output() #presents '_' and numbers to user
+    word_output(game_d) #presents "_ _ _" and numbers under to player
     regexp = create_regexp(game_d) #creates regexp from game_d
-    corpus_d = mod_corpus_d(regexp, corpus_d) #TODO not sure if this is right
-    print("length of corpus:", len(corpus_d)) #remove this before exam
-    if len(corpus_d) < 100:
-        print(corpus_d)
-    else:
-        pass
-    most_common_letter = letter_freq() #counts letter freq in corpus dictionary, returns most common letter
+    corpus_d = mod_corpus_d(regexp, corpus_d) #modifies corpus_d with regular expression
+    most_common_letter, used_letters = letter_freq(corpus_d, used_letters) #counts letter freq in corpus dictionary, returns most common letter
     num_guesses += 1
-    print("Guess number", num_guesses)
-    rightWrong, placement_l = make_guess(most_common_letter)
-    # print(placement_l)
-    if placement_l == "done":
+    print("Guess number " + str(num_guesses) + ":") #presents number of guesses to player
+    rightWrong, feedback = make_guess(most_common_letter)
+    if feedback == "done":
         print("I WON!!! With only", num_guesses, "tries!")
         break
     else:
-        if rightWrong == 1:
-            #corpus_d = mod_corpus_d() #TODO not sure if this is right, haven't tried it...
-            game_d = mod_game_d(placement_l, most_common_letter)
-        else:
-            corpus_d = mod_corpus_d(regexp, corpus_d)
-print("done")
-
-#TODO fix regexp to eliminate words with already confirmed letter in other places in word
+        if rightWrong == "yes":
+            placement_l = feedback.split() #creates a list of placements, from str
+            game_d = mod_game_d(placement_l, most_common_letter) #adds new knowned letters to game_d
+        elif rightWrong == "no":
+            corpus_d = mod_corpus_d(feedback, corpus_d) #removes all words containing faulty letter from corpus_d
+print("end")
--- hang_lady_prepare_game.py
+++ hang_lady_prepare_game.py
@@ -1,7 +1,7 @@
 import random
 
 def ask_letters():
-    """Different ways to ask for starting letters"""
+    """Outputs to ask for starting letters"""
     choices = ["How many letters are in the word? \n",
                "Number of letters? \n",
                "How many letters? \n",
@@ -11,7 +11,7 @@ def ask_letters():
     return random.choice(choices)
 
 def not_int():
-    """Different ways to ask for an int"""
+    """Outputs to ask for an int, if ValueError"""
     choices = ["Need you to write a positive number. \n",
                "Just a number, please. \n",
                "Not an integer. Try again. \n"]
@@ -22,8 +22,8 @@ def prepare_game():
     num_letters = 0
     try:
         num_letters = int(input(ask_letters()))
-        return num_letters
-    except ValueError:
+        return num_letters #returns number of letters in word
+    except ValueError: #if ValueError, give error message and start over
         print(not_int())
         return prepare_game()
 
@@ -34,34 +34,32 @@ def create_dic(num_letters):
     while i <= num_letters:
         game_d[i] = "_"
         i += 1
-    return game_d
+    return game_d #returns dictionary {1: '_', 2: '_' ...}
 
 def start_game():
     """Calls the first functions, to start a game"""
     name = input('Hi human person! What is your "name"?\n') #asks for player's name
     input("Hi " + name + "! Think of a hang-lady-type word. Okay?\n (Press enter when ready)") # greets the player
     num_letters = prepare_game() # gets the length of the word
-    game_d = create_dic(num_letters) # creates suitable length dictionary with "_" values
+    game_d = create_dic(num_letters) # creates dictionary with "_" values
     return game_d #returns dictionary with '_' as values
 
 def create_corpus():
     """ID's right len words from original corpus and creates corpus dictionary"""
-    corpus_d = {} # dictionary with all words with right length
+    corpus_d = {}
     with open('./project_korpusar/only_words.conll', 'r') as fin:
         for line in fin:
             line = line.rstrip('\n')
-            if len(line) == num_letters:
-                line = line.lower()
-                # print(line) # checks if the program works to this point
+            if len(line) == len(game_d): #if word in corpus is same length as game word
+                line = line.lower() #remove capital letters
                 if line not in corpus_d:
-                    corpus_d[line] = 1
+                    corpus_d[line] = 1 #adds entry {word: 1}
                 else:
-                    corpus_d[line] = corpus_d[line] + 1
+                    corpus_d[line] = corpus_d[line] + 1 #adds 1 to value = {word: 2}(1+1)
             else:
-                pass
+                pass #if word isn't right length - ignore the word
     fin.close()
-    return corpus_d
+    return corpus_d #returns a dictionary containing all words with right length
 
-game_d = start_game() # initiates, created 
-num_letters = len(game_d)
-corpus_d = create_corpus()
+game_d = start_game() #initiates game. Gets number of letters. Creates game_d.
+corpus_d = create_corpus() #creates dictionary {cow: 3, fly: 2, bug:45, ape: 10 ...}
